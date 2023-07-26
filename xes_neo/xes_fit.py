91caade2e1b3495fe48abf8089ffd7d5ac2bfbaa
Demonstrations <demos@dhcp230.merunorth104.iit.edu>
2023-07-26 11:54:10 -0500

    With sqrt(cts)

--- xes_neo/xes_fit.py
+++ xes_neo/xes_fit.py
@@ -261,8 +261,9 @@ class background():
 
     def mutate_background_val(self,chance):
         if random.random()*100 < chance:
+        	print(self.background)
             self.background = np.random.choice(self.backgroundRange)
-
+			print(self.background)
 
 
     #Make sure to add in each background here 
